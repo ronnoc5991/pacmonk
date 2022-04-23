1a02454a08479b0703ee54b67e82441947a54441
Connor Streng <connor.streng@mediamonks.com>
2022-04-23 08:17:24 +0200

    Add ToDos, comment out grid checker

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,35 @@ import { Game } from "./classes/Game";
 import { monsterConfig } from "./config/monster";
 import { config } from "./config/config";
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+// const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-const templateContainer = document.createElement("div");
-templateContainer.style.width = `${canvas.width}px`;
-templateContainer.style.height = `${canvas.height}px`;
-templateContainer.classList.add("template-container");
-templateContainer.classList.add("is-hidden");
-mazeTemplate.forEach((row) => {
-  row.forEach((cell) => {
-    const cellDiv = document.createElement("div");
-    cellDiv.style.width = `${canvas.width / row.length}px`;
-    cellDiv.style.height = `${canvas.height / mazeTemplate.length}px`;
-    cellDiv.innerText = cell;
-    templateContainer.appendChild(cellDiv);
-  });
-});
-
-document.body.appendChild(templateContainer);
-
-const gridToggleButton = document.getElementById("grid-toggle");
-gridToggleButton?.addEventListener("click", (event) => {
-  event.preventDefault();
-  templateContainer.classList.toggle("is-hidden");
-});
+// const templateContainer = document.createElement("div");
+// templateContainer.style.width = `${canvas.width}px`;
+// templateContainer.style.height = `${canvas.height}px`;
+// templateContainer.classList.add("template-container");
+// templateContainer.classList.add("is-hidden");
+// mazeTemplate.forEach((row) => {
+//   row.forEach((cell) => {
+//     const cellDiv = document.createElement("div");
+//     cellDiv.style.width = `${canvas.width / row.length}px`;
+//     cellDiv.style.height = `${canvas.height / mazeTemplate.length}px`;
+//     cellDiv.innerText = cell;
+//     templateContainer.appendChild(cellDiv);
+//   });
+// });
+//
+// document.body.appendChild(templateContainer);
+//
+// const gridToggleButton = document.getElementById("grid-toggle");
+// gridToggleButton?.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   templateContainer.classList.toggle("is-hidden");
+// });
 
 const mazeTemplates = [mazeTemplate];
+
+// TODO: Create a cheat code mode
+// if you type in the password 'websites', you unlock 'Monk Mode' and the characters change into a monk vs ninjas
 
 // TODO: create multiple config objects for multiple games
 // ie: PacMan, Ms Pacman, etc
